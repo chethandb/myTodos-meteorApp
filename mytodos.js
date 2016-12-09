8cba3fbe59db65dd5b32699cb6498f08e8729772
chethan <cdevarahallibalakr@unomaha.edu>
2016-12-09 02:02:12 -0600

    step 14_1

--- mytodos.js
+++ mytodos.js
@@ -73,14 +73,16 @@ if(Meteor.isClient){
     });
 
     Template.addTodo.events({
-        
+        //add todo event
         'submit form': function(event){
             event.preventDefault();
             var todoName = $('[name="todoName"]').val();
+            var currentList = this._id;
             Todos.insert({
                 name: todoName,
                 completed: false,
-                createdAt: new Date()
+                createdAt: new Date(),
+                listId: currentList
             });
             $('[name="todoName"]').val('');
         }
