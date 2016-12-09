f1f556ce275830f2dc81325e3a039a87f2eea685
chethan <cdevarahallibalakr@unomaha.edu>
2016-12-09 03:59:35 -0600

    step 18

--- mytodos.js
+++ mytodos.js
@@ -36,6 +36,30 @@ Lists = new Meteor.Collection('lists');
 
 if(Meteor.isClient){
 
+    //validation code
+    $.validator.setDefaults({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 6
+            }
+        },
+        messages: {
+            email: {
+                required: "You must enter an email address.",
+                email: "You've entered an invalid email address."
+            },
+            password: {
+                required: "You must enter a password.",
+                minlength: "Your password must be at least {0} characters."
+            }
+        }
+    });
+
     //helper functions
 
     //helper to find my todos
@@ -79,24 +103,64 @@ if(Meteor.isClient){
         }
     });
 
+    //render functions
+    Template.login.onRendered(function(){
+        var validator = $('.login').validate({
+            submitHandler: function(event){
+                var email = $('[name=email]').val();
+                var password = $('[name=password]').val();
+                Meteor.loginWithPassword(email, password, function(error){
+                    if(error){
+                        if(error.reason == "User not found"){
+                            validator.showErrors({
+                                email: "That email doesn't belong to a registered user."   
+                            });
+                        }
+                        if(error.reason == "Incorrect password"){
+                            validator.showErrors({
+                                password: "You entered an incorrect password."    
+                            });
+                        }
+                    } else {
+                        var currentRoute = Router.current().route.getName();
+                        if(currentRoute == "login"){
+                            Router.go("home");
+                        }
+                    }
+                });
+            }
+        });
+    });
+
+    Template.register.onRendered(function(){
+        var validator = $('.register').validate({
+            submitHandler: function(event){
+                var email = $('[name=email]').val();
+                var password = $('[name=password]').val();
+                Accounts.createUser({
+                    email: email,
+                    password: password
+                }, function(error){
+                    if(error){
+                        if(error.reason == "Email already exists."){
+                            validator.showErrors({
+                                email: "That email already belongs to a registered user."   
+                            });
+                        }
+                    } else {
+                        Router.go("home");
+                    }
+                });
+            }    
+        });
+    });
+
     //code for events
 
     //code for register events
     Template.register.events({
         'submit form': function(event){
             event.preventDefault();
-            var email = $('[name=email]').val();
-            var password = $('[name=password]').val();
-            Accounts.createUser({
-                email: email,
-                password: password
-            }, function(error){
-                if(error){
-                    console.log(error.reason); // Output error if registration fails
-                } else {
-                    Router.go("home"); // Redirect user if registration succeeds
-                }
-            });
         }
     });
 
@@ -104,18 +168,6 @@ if(Meteor.isClient){
     Template.login.events({
         'submit form': function(event){
             event.preventDefault();
-            var email = $('[name=email]').val();
-            var password = $('[name=password]').val();
-            Meteor.loginWithPassword(email, password, function(error){
-                if(error){
-                    console.log(error.reason);
-                } else {
-                    var currentRoute = Router.current().route.getName();
-                    if(currentRoute == "login"){
-                        Router.go("home");
-                    }
-                }
-            });
         }
     });
 
